002b08605b4bb2520c5533d94ca6eaaef37edfcb
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-05-28 13:39:35 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_28_011111) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
